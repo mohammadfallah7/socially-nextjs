ac3fb1fe249d5405ce9d421a37c6190ada7727ad
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-11-25 16:17:05 +0330

    feat: :sparkles: Implement private routes

--- app/(marketing)/notifications/page.tsx
+++ app/(marketing)/notifications/page.tsx
@@ -1,4 +1,11 @@
-const Notifications = () => {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+const Notifications = async () => {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session) redirect("/sign-in");
+
   return (
     <div>
       <h1>Notifications page</h1>
