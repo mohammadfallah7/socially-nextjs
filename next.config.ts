414e04f2ee7b0532c3245b2d8d5da02f6ac50408
Mohammad Fallah <mohammadfallah.w@gmail.com>
2026-02-27 21:39:34 +0330

    feat: :sparkles: Ready for vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  output: "standalone",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
